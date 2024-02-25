fca8ec41fcc8c2b35a858db253e2f643afaaf1a8
Alexander Gidzenko <gidzenko@mail.ru>
2024-02-25 12:36:33 +0300

    add new files

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env/python3
+
+
+import brain_games.cli
+
+
+def greet():
+    print('Welcome to the Brain Games!')
+    brain_games.cli.welcome_user()
+    
+
+
+def main():
+    greet()
+    
+if __name__ == '__main__':
+    main()
